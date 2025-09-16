621b1fef6d60818651a5491f0421037e3f978d05
luisamejiac <162033786+luisamejiac@users.noreply.github.com>
2025-09-16 10:31:16 +0200

    Update login_dardel_R.sh

--- login_dardel_R.sh
+++ login_dardel_R.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-almcama@dardel.pdc.kth.se <<'ENDSSH'
+ssh almcama@dardel.pdc.kth.se <<'ENDSSH'
 cd scripts/16s-metabarcoding-ASV/output #Go to directory 
 module load PDC/24.11 # Load modules
 export LANG=C # Set safe locale to avoid R crashing
